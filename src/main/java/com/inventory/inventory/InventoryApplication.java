dc7a80ed298317fb3596c49d5fdd45b4feb7238c
ogonchar <ostap.gonchar@gmail.com>
2023-01-20 20:00:17 +0100

    Updated instruction

--- src/main/java/com/inventory/inventory/InventoryApplication.java
+++ src/main/java/com/inventory/inventory/InventoryApplication.java
@@ -1,5 +1,5 @@
 
-package com.example.inventory;
+package com.inventory.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
