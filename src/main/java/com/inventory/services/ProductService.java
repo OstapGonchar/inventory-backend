113ec0f5624e5e1ce74f0ed1a489a7727c1e6e9e
ogonchar <ostap.gonchar@gmail.com>
2023-02-28 21:10:41 +0100

    Changed & to ISO currency

--- src/main/java/com/inventory/services/ProductService.java
+++ src/main/java/com/inventory/services/ProductService.java
@@ -11,22 +11,13 @@ public class ProductService {
     private final List<Product> products = new ArrayList<>();
 
     public ProductService() {
-        products.add(new Product("1", "boiler", "heat-water boiler", 100.0, "$", 3));
-        products.add(new Product("2", "tank", "softened-water tank", 20.0, "$", 2));
-        products.add(new Product("3", "pump", "cold water pump", 40.0, "$", 3));
+        products.add(new Product("1", "boiler", "heat-water boiler", 100.0, "USD", 3));
+        products.add(new Product("2", "tank", "softened-water tank", 20.0, "USD", 2));
+        products.add(new Product("3", "pump", "cold water pump", 40.0, "USD", 3));
     }
 
     public List<Product> listOfProducts() {
         return products;
-    }
-
-    public List<Product> postProducts() {
-        ArrayList<Product> listOfNewProducts = new ArrayList<Product>();
-        listOfNewProducts.add(new Product("1", "boiler", "heat-water boiler", 100.0, "$", 3));
-        listOfNewProducts.add(new Product("2", "tank", "softened-water tank", 20.0, "$", 2));
-        listOfNewProducts.add(new Product("3", "pump", "cold water pump", 40.0, "$", 3)
-        );
-        return listOfNewProducts;
     }
 
     public void addProduct(Product product) {
@@ -36,7 +27,6 @@ public class ProductService {
     public void deleteProduct(String id) {
         int position = 0;
         boolean positionFound = false;
-       // Optional<Product> optionalProduct = products.stream().filter(product -> product.id().equals(id)).findFirst();
         for (int i = 0; i < products.size(); i++) {
             if (id.equals(products.get(i).id())) {
                 positionFound = true;
@@ -55,11 +45,6 @@ public class ProductService {
         deleteProduct(id);
         addProduct(product);
     }
-
- /*   public List<Product> addProduct(Product product) {
-        product = new Product("4", "boiler", "heat-water boiler", 100.0, "$", 3);
-        return postProducts().add(product);
-    }*/
 
 }
 
